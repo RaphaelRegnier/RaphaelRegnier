327c0aef6aa5aa477915fa5e5882db801faad120
Raphael Regnier <raphael.regnier@hotmail.fr>
2017-07-03 16:20:09 -0400

    second exercise

--- rails-longest-word-game/config/routes.rb
+++ rails-longest-word-game/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'grid/game'
+
+  get 'grid/score'
+
+  get 'game', to: "grid#game"
+  get 'score', to: "grid#score"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
